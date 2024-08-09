8cba5a7f4170f66a8ca2847cf21f1ffe6bdeb724
arjun8009 <arjunbiswas.y2k@gmail.com>
2024-08-09 20:26:49 +0100

    changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 import pickle
 import os
 import base64
+from llama_index.core.retrievers import VectorIndexRetriever
 
 app = Flask(__name__)
 
@@ -14,6 +15,13 @@ def home():
 @app.route('/api/get-index', methods=['GET'])
 def get_index():
     chunking_method = request.args.get('chunking_method', 'default')
+    similarity_top_k = int(request.args.get('similarity_top_k', None))
+    metadata = request.args.getlist('metadata', None)
+    queries = request.args.getlist('queries', None)
+    metadata = [int(i) for i in metadata]
+    print(metadata)
+    print(queries)
+
     # Example parameter
     path = os.path.join(os.getcwd(), "vector_index_pickle")
     file_path = os.path.join(path, f"{chunking_method}_vector_index.pkl")
@@ -27,9 +35,13 @@ def get_index():
         index = pickle.load(file)
 
     # Serialize the index to base64
-    index_serialized = base64.b64encode(index).decode('utf-8')
+    node_ids = [node.id_ for node in index.docstore.docs.values() if int(node.metadata['file_id']) in metadata] if type(metadata) == list else None
+    print("Node ids",node_ids)
+    retriever = VectorIndexRetriever(index, node_ids=node_ids, similarity_top_k=similarity_top_k)
+    print("retriever-response",{i : retriever.retrieve(query) for i, query in enumerate(queries)})
+    retrieved_nodes = base64.b64encode(pickle.dumps({i : retriever.retrieve(query) for i, query in enumerate(queries)})).decode('utf-8')
 
-    return jsonify({"vector_index": index_serialized})
+    return jsonify({"vector_index": retrieved_nodes})
 
 
 if __name__ == '__main__':
