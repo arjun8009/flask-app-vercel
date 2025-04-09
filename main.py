c6309da13a5a7a290561e1ea3948c410142d1bcd
arjun8009 <arjunbiswas.y2k@gmail.com>
2025-04-09 13:30:33 +0100

    changes

--- main.py
+++ main.py
@@ -16,39 +16,43 @@ def home():
 
 @app.route('/api/get-index', methods=['GET'])
 def get_index():
-    chunking_method = request.args.get('chunking_method', 'default')
-    similarity_top_k = int(request.args.get('similarity_top_k', None))
-    metadata = request.args.getlist('metadata', None)
-    queries = request.args.getlist('queries', None)
-    metadata = [int(i) for i in metadata]
-    print(metadata)
-    print(queries)
+    try:
+        chunking_method = request.args.get('chunking_method', 'default')
+        similarity_top_k = int(request.args.get('similarity_top_k', None))
+        metadata = request.args.getlist('metadata', None)
+        queries = request.args.getlist('queries', None)
+        queries = [i for i in queries if i!='']
+        metadata = [int(i) for i in metadata]
+        print(metadata)
+        print(queries)
 
-    # Example parameter
-    path = os.path.join(os.getcwd(), "vector_index_pickle")
-    file_path = os.path.join(path, f"{chunking_method}_vector_index.pkl")
+        # Example parameter
+        path = os.path.join(os.getcwd(), "vector_index_pickle")
+        file_path = os.path.join(path, f"{chunking_method}_vector_index.pkl")
 
-    # Check if the file exists
-    if not os.path.exists(file_path):
-        return jsonify({"error": "File not found"}), 404
+        # Check if the file exists
+        if not os.path.exists(file_path):
+            return jsonify({"error": "File not found"}), 404
 
-    # Load the pickled object
-    with open(file_path, "rb") as file:
-        index = pickle.load(file)
+        # Load the pickled object
+        with open(file_path, "rb") as file:
+            index = pickle.load(file)
 
-    # Serialize the index to base64
-    node_ids = [node.id_ for node in index.docstore.docs.values() if int(node.metadata['file_id']) in metadata] if type(metadata) == list else None
-    print("Node ids",node_ids)
-    if len(node_ids) == 0 or node_ids == None:
-        print("1st pipeline entering")
-        retriever = VectorIndexRetriever(index, similarity_top_k=similarity_top_k)
-    else:
-        print("other pipelines entering")
-        retriever = VectorIndexRetriever(index,node_ids=node_ids,similarity_top_k=similarity_top_k)
-    print("retriever-response",{i : retriever.retrieve(query) for i, query in enumerate(queries)})
-    retrieved_nodes = base64.b64encode(pickle.dumps({i : retriever.retrieve(query) for i, query in enumerate(queries)})).decode('utf-8')
+        # Serialize the index to base64
+        node_ids = [node.id_ for node in index.docstore.docs.values() if int(node.metadata['file_id']) in metadata] if type(metadata) == list else None
+        print("Node ids",node_ids)
+        if len(node_ids) == 0 or node_ids == None:
+            print("1st pipeline entering")
+            retriever = VectorIndexRetriever(index, similarity_top_k=similarity_top_k)
+        else:
+            print("other pipelines entering")
+            retriever = VectorIndexRetriever(index,node_ids=node_ids,similarity_top_k=similarity_top_k)
+        print("retriever-response",{i : retriever.retrieve(query) for i, query in enumerate(queries)})
+        retrieved_nodes = base64.b64encode(pickle.dumps({i : retriever.retrieve(query) for i, query in enumerate(queries)})).decode('utf-8')
 
-    return jsonify({"vector_index": retrieved_nodes})
+        return jsonify({"vector_index": retrieved_nodes})
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
